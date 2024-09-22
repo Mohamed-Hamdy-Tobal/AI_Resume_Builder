273d14da34c199601224f6213649300e76c0a571
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-22 11:38:17 +0300

    Add User Resumes To Dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import AddResume from '@/components/features/dashboard/AddResume'
+import UserResumes from '@/components/features/dashboard/UserResumes'
 import React from 'react'
 
 const Dashboard = () => {
@@ -9,6 +10,7 @@ const Dashboard = () => {
                 <p className='text-[#999]'>Start Creating AI Resume to your next job role</p>
                 <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 mt-10'>
                     <AddResume />
+                    <UserResumes/>
                 </div>
             </div>
         </div>
