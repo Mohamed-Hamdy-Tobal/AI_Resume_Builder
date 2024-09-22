f5dea9f6998e01f7797ad044bc2ff838bc731363
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-22 11:36:47 +0300

    User Resumes Hook

---
+++ src/hooks/userUserResume.js
@@ -0,0 +1,35 @@
+import api from '@/lib/api';
+import { useUser } from '@clerk/clerk-react';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+const userUserResume = () => {
+
+    const { isSignedIn, user } = useUser()
+
+    const [userResume, setUserResume] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchUserResumes = async () => {
+            try {
+                const response = await api.get(`/api/user-resumes?filters[userEmail][$eq]=${user?.primaryEmailAddress.emailAddress}`);
+                setUserResume(response.data.data);
+            } catch (err) {
+                setError(err);
+                toast.error("Error During Get Your Resumes!")
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        if (isSignedIn) {
+            fetchUserResumes();
+        }
+    }, [isSignedIn]);
+
+    return { userResume, loading, error };
+};
+
+export default userUserResume;
