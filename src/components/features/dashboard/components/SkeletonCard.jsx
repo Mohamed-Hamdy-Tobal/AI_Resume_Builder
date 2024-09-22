e1b81e97f829eec434ed8efd180f027055f71912
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-22 11:37:38 +0300

    SkeletonCard

---
+++ src/components/features/dashboard/components/SkeletonCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Skeleton } from '@/components/ui/skeleton'
+
+const SkeletonCard = () => {
+    return (
+        <div className="flex flex-col space-y-3">
+            <Skeleton className="h-[150px] w-full rounded-xl" />
+            <div className="space-y-2">
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="h-4 w-3/4" />
+            </div>
+        </div>
+    )
+}
+
+export default SkeletonCard
