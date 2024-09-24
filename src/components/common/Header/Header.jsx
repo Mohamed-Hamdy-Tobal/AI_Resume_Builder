c89b76a80782ad02c0231cba329ac1042b92eecd
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-24 10:06:57 +0300

    Refactor Header

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -8,17 +8,21 @@ const Header = () => {
     const user = useUser()
 
     return (
-        <div className='py-3 px-5 flex justify-between items-center shadow-md'>
-            <Link to={'/'}><img src='/logo.svg' className='w-[100px]' /></Link>
-            {user.isSignedIn ? (
-                <div className='flex justify-start items-center gap-3'>
-                    <Link to={'/dashboard'}><Button variant="outline">Dashboard</Button></Link>
-                    <UserButton />
+        <header className='py-3 shadow-md'>
+            <div className="container">
+                <div className='flex justify-between items-center'>
+                    <Link to={'/'}><img src='/logo.svg' className='w-[100px]' /></Link>
+                    {user.isSignedIn ? (
+                        <div className='flex justify-start items-center gap-3'>
+                            <Link to={'/dashboard'}><Button variant="outline">Dashboard</Button></Link>
+                            <UserButton />
+                        </div>
+                    ) : (
+                        <Link to={'/auth/sign-in'}><Button>Get Started</Button></Link>
+                    )}
                 </div>
-            ) : (
-                <Link to={'/auth/sign-in'}><Button>Get Started</Button></Link>
-            )}
-        </div>
+            </div>
+        </header>
     )
 }
 
