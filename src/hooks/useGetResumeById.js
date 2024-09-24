5e2e5bf997fe0c9c5af59966bfa42118fc9069a3
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-24 10:08:18 +0300

    Get Resume By id hook

---
+++ src/hooks/useGetResumeById.js
@@ -0,0 +1,36 @@
+import api from '@/lib/api';
+import { useUser } from '@clerk/clerk-react';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+const useGetResumeById = (id) => {
+
+    const { isSignedIn } = useUser()
+
+    const [resume, setResume] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchResume = async () => {
+            try {
+                const response = await api.get(`/api/user-resumes/${id}`);
+                setResume(response.data.data);
+            } catch (err) {
+                setError(err);
+                console.log("error:",err)
+                toast.error("Error During Get Your Resumes!", err.error.message)
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        if (isSignedIn) {
+            fetchResume();
+        }
+    }, [isSignedIn]);
+
+    return { resume, loading, error };
+};
+
+export default useGetResumeById;
