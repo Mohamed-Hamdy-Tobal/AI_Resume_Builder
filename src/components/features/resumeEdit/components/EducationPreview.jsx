4d8cef992f10e1e886b38a5dec55b99da6e24dd2
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-24 10:10:05 +0300

    Education Section Component

---
+++ src/components/features/resumeEdit/components/EducationPreview.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const EducationPreview = ({ resumeInfo }) => {
+    return (
+        <div>
+            <h1
+                className='font-bold text-sm text-center mb-2'
+                style={{
+                    color: resumeInfo?.themeColor
+                }}
+            >
+                Education
+            </h1>
+            <hr style={{
+                borderColor: resumeInfo?.themeColor
+            }} />
+
+            <div>
+                {resumeInfo?.education.map((education, index) => (
+                    <div key={index} className='my-5'>
+                        <h1 className='text-sm font-bold' style={{
+                            color: resumeInfo?.themeColor
+                        }}>{education.universityName}</h1>
+                        <p className='text-xs flex justify-between items-center'>
+                            {education.degree} in {education.major}
+                            <span>{education.startDate} - {education.endDate}</span>
+                        </p>
+                        <p className='text-xs my-2'>{education.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default EducationPreview
