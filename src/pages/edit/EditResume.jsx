0fdfbf271787df78a8613e88de06e291d61d6cda
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-24 10:11:41 +0300

    Edit Resume Page

--- src/pages/edit/EditResume.jsx
+++ src/pages/edit/EditResume.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import FormSection from '@/components/features/resumeEdit/FormSection'
+import PreviewSection from '@/components/features/resumeEdit/PreviewSection'
+import { ResumeContext } from '@/context/ResumeContext'
+import dummy from '@/data/dummy'
+import useGetResumeById from '@/hooks/useGetResumeById'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const EditResume = () => {
 
-    const param = useParams()
+    const {resumeId} = useParams()
 
-    console.log("param:",param.resumeId)
+    const { resume, loading } = useGetResumeById(resumeId);
+
+    const [resumeInfo, setResumeInfo] = useState()
+
+    useEffect(() => {
+        setResumeInfo(dummy)
+    }, [])
+
+    // console.log("resume:", resume)
 
     return (
-        <div>EditResume</div>
+        <ResumeContext.Provider value={{resumeInfo, setResumeInfo}}>
+            <div className="edit-page py-10">
+                <div className="container">
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+                        <FormSection />
+                        <PreviewSection />
+                    </div>
+                </div>
+            </div>
+        </ResumeContext.Provider>
     )
 }
 
