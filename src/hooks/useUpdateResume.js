46c6278923bf275fe3d8039588285aaef7c1234a
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-25 12:21:41 +0300

    Update Context with sending resume data

---
+++ src/hooks/useUpdateResume.js
@@ -0,0 +1,60 @@
+import { useContext, useState } from 'react'
+import api from '@/lib/api'
+import { useUser } from '@clerk/clerk-react'
+import { useToast } from "@/hooks/use-toast"
+import { ResumeContext } from '@/context/ResumeContext'
+
+export const useUpdateResume = (id) => {
+
+    const { toast } = useToast()
+
+    const { setResumeInfo } = useContext(ResumeContext)
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [isSuccess, setIsSuccess] = useState(false)
+    const [isError, setIsError] = useState(false)
+    const [data, setData] = useState(null)
+
+    const updateResume = async (dataResume) => {
+
+        setIsLoading(true)
+
+        const formData = {
+            data: {
+                ...dataResume,
+            }
+        }
+
+        console.log("formData:", formData)
+
+        try {
+            const response = await api.put(`/api/user-resumes/${id}/`, formData)
+            console.log("Response cart:", response)
+            setData(response.data.data)
+            setIsSuccess(true)
+            setResumeInfo((prev) => ({
+                ...prev,
+                ...response.data.data
+            }));
+            toast({
+                title: "Successfully send data",
+                description: "Your data has been updated successfully",
+            })
+            return response.data
+        } catch (error) {
+            setIsLoading(false)
+            setIsError(true)
+            console.error('Failed to add to cart', error)
+            toast({
+                variant: "destructive",
+                title: "Uh oh! Something went wrong.",
+                description: "There was a problem with your request.",
+            })
+            return Promise.reject(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    return { updateResume, isLoading, isSuccess, isError, data }
+}
