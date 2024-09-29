55abe264e1bc669a19ffb3fa9896465ddbadf7a9
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-29 10:13:42 +0300

    Add Skeleton Form Preview Data

---
+++ src/components/features/resumeEdit/components/SkeletonResume.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Skeleton } from '@/components/ui/skeleton'
+
+const SkeletonResume = () => {
+    return (
+        <div className="flex flex-col space-y-3 w-full">
+            <Skeleton className="h-[100px] w-full rounded-xl" />
+            <div className="space-y-2">
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="h-4 w-3/4" />
+            </div>
+            <div className="space-y-2">
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="h-4 w-3/4" />
+            </div>
+            <Skeleton className="h-[100px] w-full rounded-xl" />
+            <div className="space-y-2">
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="h-4 w-3/4" />
+            </div>
+            <Skeleton className="h-[100px] w-full rounded-xl" />
+            <div className="space-y-2">
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="h-4 w-3/4" />
+            </div>
+        </div>
+    )
+}
+
+export default SkeletonResume
