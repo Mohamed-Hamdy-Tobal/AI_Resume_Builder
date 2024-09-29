294825398a1a14a661656ab3e4d15063a8e48557
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-09-29 11:26:42 +0300

    Enhance Show Experiences Data Only After add any info to the form

--- src/components/features/resumeEdit/previewComponents/ProfessionalExperiencesPreview.jsx
+++ src/components/features/resumeEdit/previewComponents/ProfessionalExperiencesPreview.jsx
@@ -1,10 +1,13 @@
 import dummy from '@/data/dummy'
 import { formatDate } from '@/utils/formatDate'
+import { hasTruthyValue } from '@/utils/hasTruthyValue'
 import React from 'react'
 
 const ProfessionalExperiencesPreview = ({ resumeInfo }) => {
 
     const customExperiences = resumeInfo?.experiences.length > 0 ? resumeInfo : dummy
+
+    console.log("customExperiences:", customExperiences)
 
     return (
         <div className='mt-5'>
@@ -20,21 +23,29 @@ const ProfessionalExperiencesPreview = ({ resumeInfo }) => {
                 borderColor: resumeInfo?.themeColor
             }} />
             <div className='mt-3'>
-                {customExperiences?.experiences?.map((experience, index) => (
-                    <div key={index} className='my-5'>
-                        <h1 className='text-sm font-bold' style={{
-                            color: resumeInfo?.themeColor
-                        }}>{experience.title}</h1>
-                        <h2 className='text-xs flex justify-start items-start sm:justify-between sm:items-center flex-wrap'>
-                            {experience.companyName}, {experience.city}, {experience.state}
-                            <span>
-                                {formatDate(experience.startDate)} - {experience.currentlyWorking ? "Present" : formatDate(experience.endDate)}
-                            </span>
-                        </h2>
-                        {/* <p className='text-xs my-2'>{experience.workSummary}</p> */}
-                        <div dangerouslySetInnerHTML={{__html: experience.workSummary}}/>
-                    </div>
-                ))}
+                {customExperiences?.experiences?.map((experience, index) => {
+                    const toShow = hasTruthyValue(experience)
+                    if (!toShow) return null
+                    return (
+                        <div key={index} className='my-5'>
+                            {experience.title && (<h1 className='text-sm font-bold' style={{
+                                color: resumeInfo?.themeColor
+                            }}>{experience.title}</h1>)}
+                            {(experience.companyName
+                                || experience.city
+                                || experience.state
+                                || experience.startDate
+                                || experience.endDate) && (<h2 className='text-xs flex justify-start items-start sm:justify-between sm:items-center flex-wrap'>
+                                    {experience.companyName}, {experience.city}, {experience.state}
+                                    <span>
+                                        {formatDate(experience.startDate)} - {experience.currentlyWorking ? "Present" : formatDate(experience.endDate)}
+                                    </span>
+                                </h2>)}
+                            {/* <p className='text-xs my-2'>{experience.workSummary}</p> */}
+                            {experience.workSummary && (<div className='mt-1 text-sm' dangerouslySetInnerHTML={{ __html: experience.workSummary }} />)}
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
