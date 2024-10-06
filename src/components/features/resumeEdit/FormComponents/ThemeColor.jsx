b1ab5e0e8e73c41575d74b6384e3c2052c3a2c25
Mohamed Tobal <mohamedtobal369@gmail.com>
2024-10-06 10:44:44 +0300

    Add Color Picker To The Theme Color Of Resume

--- src/components/features/resumeEdit/FormComponents/ThemeColor.jsx
+++ src/components/features/resumeEdit/FormComponents/ThemeColor.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { LayoutGrid } from 'lucide-react'
 import {
@@ -10,18 +10,25 @@ import { resumeThemeColors } from '@/constant/ThemeColors'
 import { useUpdateResume } from '@/hooks/useUpdateResume'
 import { useParams } from 'react-router-dom'
 import { ResumeContext } from '@/context/ResumeContext'
+import { ColorPicker } from '@/components/ui/color-picker'
 
 const ThemeColor = () => {
 
     const { resumeId } = useParams()
 
+
     const { updateResume } = useUpdateResume(resumeId)
     const { resumeInfo, setResumeInfo } = useContext(ResumeContext)
 
+    const [color, setColor] = useState(resumeInfo?.resume_theme);
+    console.log("color:", color)
+
     const handleColor = (color) => {
+        if (resumeInfo?.resume_theme === color) return
+        
         console.log("color:", color)
         const finalData = {
-            resume_theme:color
+            resume_theme: color
         }
         setResumeInfo((prev) => ({
             ...prev,
@@ -30,21 +37,39 @@ const ThemeColor = () => {
         updateResume(finalData)
     }
 
+    useEffect(() => {
+        setColor(resumeInfo?.resume_theme)
+    }, [setColor, resumeInfo])
+
     return (
         <Popover>
             <PopoverTrigger asChild>
                 <Button variant="outline" className='flex gap-2' size="sm"> <LayoutGrid /> Theme</Button>
             </PopoverTrigger>
             <PopoverContent className="w-80">
-                <h2 className='mb-3 text-sm text-[#999]'>Select Theme Color : </h2>
-                <div className="flex gap-4 flex-wrap justify-start items-center">
-                    {resumeThemeColors.map((color) => (
-                        <span
-                            onClick={() => handleColor(color)}
-                            style={{ backgroundColor: color }}
-                            className={`rounded-full w-8 h-8 cursor-pointer border border-white hover:outline-dashed transition-all ${resumeInfo?.resume_theme == color && "outline-dashed"}`}
-                        ></span>
-                    ))}
+                <div className='w-full'>
+                    <h2 className='mb-3 text-sm text-[#999]'>Select Theme Color : </h2>
+                    <div className="flex gap-4 flex-wrap justify-start items-center">
+                        {resumeThemeColors.map((color) => (
+                            <span
+                                onClick={() => handleColor(color)}
+                                style={{ backgroundColor: color }}
+                                className={`rounded-full w-8 h-8 cursor-pointer border border-white hover:outline-dashed transition-all ${resumeInfo?.resume_theme == color && "outline-dashed"}`}
+                            ></span>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <div className='w-full mt-8 flex justify-between items-start gap-3'>
+                        <div>
+                            <ColorPicker
+                                value={color}
+                                onChange={setColor}
+                            />
+                        </div>
+                        <Button disabled={resumeInfo?.resume_theme === color} onClick={() => handleColor(color)}>Add Color</Button>
+                    </div>
+                    <p className='mt-2 flex justify-start items-center gap-2 w-full'><span>Selected Color:</span> <span>{color}</span></p>
                 </div>
             </PopoverContent>
         </Popover>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ColorPicker } from '@/components/ui/color-picker';
 
 const Home = () => {
+    const [color, setColor] = useState('#FFFFFF'); // Default color state
 
     return (
         <div>
-            Home Page
+            <div className="container">
+                <ColorPicker
+                    value={color}
+                    onChange={setColor} // Update state when color changes
+                />
+                <p>Selected Color: {color}</p>
+            </div>
         </div>
     );
 };
